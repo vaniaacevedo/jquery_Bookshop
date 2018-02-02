dd3c8ac7abfac90f9b11ca876a285384524321dd
vaniaacevedo <vaniaacevedo@me.com>
2018-02-02 12:28:30 +0100

    install the live server

--- js/shop.js
+++ js/shop.js
@@ -1,151 +1,221 @@
-//alert('hello');
-
-//Helper
-//https://cmatskas.com/get-url-parameters-using-javascript/
-
-var parseQueryString = function(url) {
-    var urlParams = {};
-    url.replace(
-        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-        function($0, $1, $2, $3) {
-            urlParams[$1] = $3;
-        }
-    );
-
-    return urlParams;
-
-}
-
-var urlParams = parseQueryString(location.search);
-console.log(urlParams);
-
-// Categories
-
 function CategoryManager() {
 
-    this.categories = [];
-    this.url = "http://api.origin.berlin/category";
-    this.currentCategory = false;
-    this.menu = $("#category-menu");
+  this.categories = [];
+  this.currentCategory = false;
+  this.url = "http://api.origin.berlin/category";
+  this.menu = $("#category-menu");
 
-    this.loadCategories = function() {
-        var that = this;
-        $.getJSON(this.url, function(data) {
-            //console.log(data);
-            //console.log(that);
-            //console.log(this);
-            that.categories = data;
-            that.addMenuItems();
-            that.getCurrentCategory();
-        })
-    }
+  this.init = function() {
+    this.loadCategories();
+  }
 
-    this.addMenuItems = function() {
-        var that = this;
-        //go through categories
-        $.each(this.categories, function(index, category) {
-            //console.log(category);
-            // add dinamically the name of each link to the navigation menu
-            var li = $("<li>").addClass("nav-item");
-            var a = $("<a>").addClass("nav-link").attr("href", "category.html?catID=" + category.id).text(category.name).appendTo(li);
-            that.menu.append(li);
-        });
+  this.loadCategories = function() {
+    var that = this;
+    $.getJSON(this.url, function(data) {
+      that.categories = data;
+      that.addMenuItems();
+      that.setCurrentCategory();
+    })
+    $(document).trigger("category_load");
+  }
 
-    }
+  this.addMenuItems = function() {
+    var that = this;
 
-    this.getCurrentCategory = function() {
-        var that = this;
-        if (!urlParams.catID) return; // break if there is now param in the URL!
+    $.each(this.categories, function(index, category) {
+      var li = $("<li>").addClass("nav-item");
+      var a = $("<a>").addClass("nav-link").attr("href", "category.html?catID=" + category.id).text(category.name).appendTo(li);
+      that.menu.append(li);
+    });
+    $(document).trigger("category_menuAdded");
+  }
 
-        // if (urlParams.catID) {
-        //   console.log('category selected')
-        // } else {
-        //   console.log('category not selected');
-        // }
+  this.setCurrentCategory = function() {
+    var that = this;
 
-        console.log('should search for category');
-        $.each(this.categories, function(index, category) {
-            console.log(category.id);
-            if (category.id == urlParams.catID) {
-                console.log('found!');
-                that.currentCategory = category;
-                console.log(category.name);
-                $(".category-name").text(category.name);
-            }
+    if (!shopManager.urlParams.catID) return; // break if there is now param in the URL!
+    $.each(this.categories, function(index, category) {
+      if (category.id == shopManager.urlParams.catID) {
+        that.currentCategory = category;
+        $(".category-name").text(category.name);
+      }
+    });
 
-        });
-
-    }
+    $(document).trigger("category_currentSet");
+  }
 
 }
-
-var categoryManager = new CategoryManager();
-categoryManager.loadCategories();
-//console.log(categoryManager);
 
 function BookManager() {
 
-    this.url = "http://api.origin.berlin/book";
-    this.book = [];
-    this.container = $("#book-container");
-    this.currentBook= false;
+  this.currentBook = false;
+  this.url = "http://api.origin.berlin/book";
+  this.books = [];
+  this.container = $("#book-container");
 
-    this.loadBooks = function() {
-        var that = this;
+  this.init = function() {
+    var that = this;
 
-        $.getJSON(this.url, function(data) {
-            that.books = data;
-            //console.log(that);
-            that.addBooks();
-            that.getCurrentBook();
+    $(document).on("category_currentSet", function() {
+      that.addBooks();
+    });
 
-        });
-    }
+    this.loadBooks();
+  }
 
-    this.addBooks = function() {
+  this.loadBooks = function() {
+    var that = this;
 
-        var that = this;
+    $.getJSON(this.url, function(data) {
+      that.books = data;
+      that.setCurrentBook();
+      that.addBooks();
+    });
+  }
 
-        $.each(this.books, function(index, book) {
+  this.addBooks = function() {
+    var that = this;
 
-            //console.log(book.image)
+    $.each(this.books, function(index, book) {
+      if (book.category_id == shopManager.categoryManager.currentCategory.id || shopManager.categoryManager.currentCategory == false) {
+        var div = $("<div>").addClass("col-3");
+        $("<img>").attr("src", book.image).addClass("img-fluid").appendTo(div);
+        var p = $("<p>").appendTo(div);
+        $("<a>").attr("href", "detail.html?book=" + book.slug).text(book.title).appendTo(p);
+        that.container.append(div);
+      }
+    })
+  }
 
-            var div = $("<div>").addClass("col-3");
-            $("<img>").attr("src", book.image).addClass("img-fluid").appendTo(div);
-            var p = $("<p>").appendTo(div);
-            $("<a>").attr("href", "detail.html?book=" + book.slug).text(book.title).appendTo(p);
-            that.container.append(div);
+  this.setCurrentBook = function() {
+    var that = this;
 
-        });
-    }
+    if (!shopManager.urlParams.book) return; // break if there is no param in the URL!
 
-    this.getCurrentBook = function() {
-        var that = this;
-        if (!urlParams.book) return; // break if there is now param in the URL
+    $.each(this.books, function(index, book) {
+      if (book.slug == shopManager.urlParams.book) {
+        that.currentBook = book;
+        $(".book-image").attr("src", that.currentBook.image);
+        $(".book-title").text(that.currentBook.title);
+        $(".book-author").text(that.currentBook.author);
+        $(".book-price").text(that.currentBook.price);
+        $(".book-year").text(that.currentBook.date);
+        $(".book-reviews").text("(" + that.currentBook.reviews + " reviews)");
+        $(".book-ratings").text(that.currentBook.rating);
+        return;
+      }
+    });
+  }
+}
 
-        //console.log ('should search for book');
-        $.each(this.books, function(index, book) {
-            //console.log(book.id);
-            if (book.slug == urlParams.book) {
-                //console.log('found!');
-                that.currentBook = book;
-                $(".book-image").attr("src",that.currentBook.image);
-                $(".book-title").text(that.currentBook.title);
-                $(".book-author").text(that.currentBook.author);
-                $(".book-price").text(that.currentBook.price);
-                $(".book-year").text(that.currentBook.date);
-                $(".book-reviews").text(that.currentBook.reviews);
-                $(".book-ratings").text(that.currentBook.ratings);
+function CartManager() {
 
+  this.cart = [];
+  this.checkoutButton = $("#checkoutButton");
+  this.container = $("#shoppingCart");
+  this.cartButton = $(".cart-amount");
 
-            }
+  this.init = function() {
+    var that = this;
+    that.cart = (localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : [];
+    that._updateCartIcon();
+    $(".cart-amount").text(this.cart.length);
 
-        });
+    $(".cart-add").on("click", function() {
+      that.addItem(shopManager.bookManager.currentBook);
+    });
+    $(".cart-checkout").on("click", function() {
+      that.checkout();
+    });
+    $(".cart-clear").on("click", function() {
+      that.clear();
+    });
+    $(document).on("click", ".cart-delete-item", function() {
+      that.deleteItem($(this).data("index"));
+    });
+    that.display();
+  }
 
+  this.display = function() {
+    var that = this;
+    var total = 0;
+    that.container.html("");
+    $.each(that.cart, function(index, item) {
+      var row = $("<tr>");
+      $("<td>").text(item.id).appendTo(row);
+      $("<td>").html($("<img>").attr("src", item.image)).appendTo(row);
+      $("<td>").text(item.title).appendTo(row);
+      $("<td>").text(item.author).appendTo(row);
+      $("<td>").text(item.price).appendTo(row);
+      var closeButton = $("<button>").addClass("btn btn-outline-warning cart-delete-item").html("&times;").attr("data-index", index);
+      $("<td>").html(closeButton).appendTo(row);
+      that.container.append(row);
+      total += item.price;
+    });
+    $(".cart-total").text(total);
+  }
 
-    }
+  this.deleteItem = function(itemIndex) {
+    this.cart.splice(itemIndex, 1);
+    this._updateStorage();
+  }
+
+  this.addItem = function(item) {
+    this.cart.push(item);
+    this._updateStorage();
+    window.location.href = "cart.html";
+  }
+
+  this.clear = function() {
+    this.cart = [];
+    this._updateStorage();
+    window.location.href = "shop.html";
+  }
+
+  this.checkout = function() {
+    alert("Thanks!");
+    this.clear();
+  }
+
+  this._updateStorage = function() {
+    localStorage.setItem("cart", JSON.stringify(this.cart));
+    this._updateCartIcon();
+    this.display();
+  }
+
+  this._updateCartIcon = function() {
+    this.cartButton.text(this.cart.length);
+    this.cartButton.parent().toggleClass("text-success", this.cart.length != 0)
+  }
 
 }
 
-var bookManager = new BookManager();
-bookManager.loadBooks();
+
+function ShopManager() {
+
+  this.cartManager = new CartManager();
+  this.bookManager = new BookManager();
+  this.categoryManager = new CategoryManager();
+  this.urlParams = {};
+
+  this.init = function() {
+    this.parseURL();
+    this.cartManager.init();
+    this.bookManager.init();
+    this.categoryManager.init();
+  }
+
+  this.parseURL = function() {
+    var that = this;
+    window.location.search.replace(
+      new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+      function($0, $1, $2, $3) {
+        that.urlParams[$1] = $3;
+      }
+    );
+  }
+
+}
+
+var shopManager = new ShopManager();
+shopManager.init();
